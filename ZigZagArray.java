898832a362f083feece4cbb4bb1b205a0fcee943
saumyakabdal <136328467+saumyakabdal@users.noreply.github.com>
2023-06-26 17:17:51 +0530

    ZigZagArray.java

---
+++ ZigZagArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class ZigZagArray {
+    static int arr[] = new int[] { 4, 3, 7, 8, 6, 2, 1 };
+
+    static void zigZag() {
+        Arrays.sort(arr);
+        for (int i = 1; i <= arr.length - 2; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        zigZag();
+        System.out.println(Arrays.toString(arr));
+    }
+}
